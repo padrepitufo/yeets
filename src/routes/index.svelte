39c4eed64e7b39c8f30fa57e3ea22f7f97475e42
Jane Streeter <jane.america.streeter@gmail.com>
2022-01-20 07:48:37 -0600

    janes first commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,5 +2,5 @@
     <title>Yeets | Welcome!</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Yeets</h1>
+<p>Yeeting all the day long!</p>
