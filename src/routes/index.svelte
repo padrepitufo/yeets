af20e99bc82949d71e3286299fdd803b5f759f97
Marc Streeter <marcstreeter@k3snena.localdomain>
2022-01-21 10:07:22 -0600

    nena justdidsomething

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
-    <title>Yeets | Welcome!</title>
+    <title>!Yeets | Welcome!</title>
 </svelte:head>
 
-<h1>Welcome to Yeets</h1>
+<h1>Welcome to Yeeters!</h1>
 <p>Yeeting all the day long!</p>
