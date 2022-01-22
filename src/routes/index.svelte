8b87adcf7c8801948965d2d97c9b63a96be5d03f
daelthegreat <98197631+daelthegreat@users.noreply.github.com>
2022-01-21 20:32:51 -0600

    the good one B)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,4 +5,4 @@
 <h1>Welcome to Yeeters in Rama Lakeline!</h1>
 <p>Yeeting all the day long! From the wonderful city of Cedar Park!</p>
 <p>This change went in at 8:26pm</p>
-<p>fortnite floss win win gang gang fortnite on topppppp all day fortnite is pushing <p>
+<p>fortnite floss win win gang gang fortnite on topppppp all day fortnite is pushing </p>
