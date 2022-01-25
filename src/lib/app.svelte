4a498f2c037cf47a4825fa590bcd845e46ac9cd4
Marc Streeter <marcstreeter@k3snena.localdomain>
2022-01-25 08:38:36 -0600

    confused

---
+++ src/lib/app.svelte
@@ -0,0 +1,12 @@
+<script>
+    let name = "Nena's Face";
+    let age = "As Old As Your Mom";
+</script>
+
+<style>
+    h1 {
+        color: purple;
+    }
+</style>
+
+<h1>Hello! I Am {name}, And I Am {age}!</h1>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
     <title>!Yeets | Welcome!</title>
-</svelte:head>
+</svelte:head> 
 
 <h1>Welcome to Yeeters!</h1>
 <p>Yeeting all the day long!</p>
