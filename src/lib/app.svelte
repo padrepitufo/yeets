1ac4fb01d399323ba171f5ad63cde3562d702cea
Marc Streeter <marcstreeter@k3snena.localdomain>
2022-01-29 11:44:28 -0600

    blorp

--- src/lib/app.svelte
+++ src/lib/app.svelte
@@ -1,6 +1,11 @@
 <script>
     let name = "Nena's Face";
     let age = "As Old As Your Mom";
+    let likes = 0;
+
+    function incrementLikes() {
+        likes += 1;
+    };
 </script>
 
 <style> 
@@ -10,3 +15,4 @@
 </style>
 
 <h1>Hello! I Am {name}, And I Am {age}!</h1>
+<button on:click="{incrementLikes}">Thumbs up {likes}</button>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,6 +1,14 @@
+<script>
+    
+</script>
+
+
 <svelte:head>
     <title>Yeets | About</title>
 </svelte:head>
 
 <h1>About</h1>
-<p>This is the beginning of an awesome site</p>
+<p>This is the beginning of an awesome site. Right now, we're learning how to program. Yayyyy /s. Here's some exampleworkcrap--</p>
+
+
+
