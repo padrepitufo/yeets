ddaa1a4628c0ce8b504690dbf551e6cbe8b72d75
jane streeter <jane.america.streeter@gmail.com>
2022-01-29 12:27:03 -0600

    quick fixes

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -3,6 +3,7 @@
         <ul>
             <li><a href="/">Yeets</a></li>
             <li><a href="/about">About</a></li>
+            <li><a href="/nena">nena</a></li>
         </ul>
     </nav>
 </header> 
