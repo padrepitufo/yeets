138fefdc78feeb9c44e5fa847600f37f30197c53
Jane Streeter <jane.america.streeter@gmail.com>
2022-02-07 11:16:39 -0600

    merged

--- svc/ui/src/routes/index.svelte
+++ svc/ui/src/routes/index.svelte
@@ -1,31 +1,19 @@
 <script>
-
 </script>
 
-<style>
-h1 {
-        color: #7ed56f;
-}
-p {
-        color: rgb(214, 116, 36);
-}
-
-    
-</style>
-
 <svelte:head>
-<<<<<<< HEAD:svc/ui/src/routes/index.svelte
-	<title>Rama Lakeline | Welcome!</title>
+	<title>Welcome!</title>
 </svelte:head>
-=======
-    <title>!Yeets | Welcome!</title>
-
-    <title>Rama Lakeline | Welcome!</title>
-</svelte:head> 
->>>>>>> 216a3150152b2540e7d92d4e7ea1b4098133b131:src/routes/index.svelte
 
 <h1>Welcome to Yeeters!</h1>
-<p>Yeeting all the day long! From the wonderful city of Cedar Park!</p>
-<p>This change went in at 8:26pm</p>
-<p>The young women are superior. Change my mind.</p>
-<img src="/img/pinkie.png" alt="pinkie">
+<p>Yeeting all the day long!</p>
+<img src="/img/pinkie.png" alt="pinkie" />
+
+<style>
+	h1 {
+		color: #7ed56f;
+	}
+	p {
+		color: rgb(214, 116, 36);
+	}
+</style>
