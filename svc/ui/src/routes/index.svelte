a6346beea452fcc09fc8e8086f0b4c08d6eed22c
Jane Streeter <jane.america.streeter@gmail.com>
2022-02-07 10:38:45 -0600

    adding new service

--- svc/ui/src/routes/index.svelte
+++ svc/ui/src/routes/index.svelte
@@ -1,7 +1,7 @@
 <svelte:head>
-    <title>Rama Lakeline | Welcome!</title>
+	<title>Rama Lakeline | Welcome!</title>
 </svelte:head>
 
-<h1>Welcome to Yeeters in Rama Lakeline!</h1>
+<h1>Welcome to Yeeters!</h1>
 <p>Yeeting all the day long! From the wonderful city of Cedar Park!</p>
 <p>This change went in at 8:26pm</p>
