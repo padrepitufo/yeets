cf60bf108d322f389e9e599eaac0c26806201a2b
nena streeter <nenastreeter@gmail.com>
2022-02-08 08:59:20 -0600

    following the udemy stuff :)

--- src/routes/nenasudemy.svelte
+++ src/routes/nenasudemy.svelte
@@ -1,6 +1,17 @@
-<script></script>
+<!-- ACTUALLY FOLLOWING UDEMY STUFF -->
+
+<script>
+    let name = 'Nena';
+    let age = 0;
+
+    function incrementAge() {
+        age += 1;
+    }
+</script>
 
 <style>
+
+
 
     @import url('https://fonts.googleapis.com/css?family=Lato|Roboto+Slab');
 
@@ -40,6 +51,11 @@
         border-color: #e40763;
         box-shadow: 1px 1px 8px rgba(77, 51, 51, 0.26);
     }
+    h1{
+        color:cadetblue;
+    }
 
 </style>
 
+<h1>Konnichiwa! Watashi wa {name}desu. {age}-saidesu! </h1>
+<button on:click="{incrementAge}">Nenrei henko!</button>
