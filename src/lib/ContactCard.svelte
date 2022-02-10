1b51265c75fdde4850bf8acdb179f47ba59c5183
ruth <ruth.ines.streeter@gmail.com>
2022-02-09 18:36:09 -0600

    Hello dad!

---
+++ src/lib/ContactCard.svelte
@@ -0,0 +1,18 @@
+<style>
+
+</style>
+
+<div>
+    <header>
+        <div>
+            <img src="" alt="">
+        </div>
+        <div>
+        </div>
+        <h1>User Name</h1>
+        <h2>Job Title</h2>
+    </header>
+</div>
+<p>CodeIng</p>
+<div>
+</div>
