cb5a0f5bede87c73f24c622889e119deb012158a
nena streeter <nenastreeter@gmail.com>
2022-04-21 17:25:34 -0500

    dads wrong but im commiting crepes :]]

--- src/lib/nenasudemy/Modal.svelte
+++ src/lib/nenasudemy/Modal.svelte
@@ -1,10 +1,33 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
+    import { createEventDispatcher, onMount, onDestroy, beforeUpdate, afterUpdate } from 'svelte';
 
     const dispatch = createEventDispatcher();
 
     let agreed = false;
+    let autoscroll = false
 
+    onMount(() => {
+        console.log('onMount');
+    });
+
+    onDestroy(() => {
+        console.log('onDestroy');
+    });
+
+    beforeUpdate(() => {
+        console.log('beforeUpdate');
+        autoscroll = agreed;
+    });
+
+    afterUpdate(() => {
+        console.log('afterUpdate');
+        if (autoscroll) {
+            const modal = document.querySelector('.modal');
+            modal.scrollTo(0, modal.scrollHeight);
+        }
+    });
+
+    console.log('Script executed');
 </script>
 
 <style>
@@ -26,7 +49,7 @@
     top: 10vh;
     left: 10%;
     width: 80%;
-    max-height: 80vh;
+    max-height: 15vh;
     background: white;
     border-radius: 5px;
     z-index: 100;
