f3a44c25216392f80ea3dd62cb4ffe6ecedbd2c5
ruth <ruth.ines.streeter@gmail.com>
2022-05-23 16:57:25 -0500

    hello dad

--- src/routes/ruthudemy.svelte
+++ src/routes/ruthudemy.svelte
@@ -1,8 +1,16 @@
 <script>
     import Header from '$lib/ruthsudemy/Header.svelte';
-    import MeetupItem from '$lib/ruthsudemy/MeetupItem.svelte';
+    import MeetupGrid from '$lib/components/MeetupGrid.svelte';
+    import TextInput from '$lib/components/TextInput.svelte';
 
-    const meetups = [
+    let title = '';
+    let subtitle = '';
+    let address = '';
+    let email = '';
+    let description = '';
+    let imgUrl = '';
+
+    let meetups = [
         {
             id: 'm1',
             title: 'Coding on a Computer 💻',
@@ -14,23 +22,111 @@
         },
         {
             id: 'm2',
-            title: '😆 Jokes to make you laugh! 🤣',
-            subtitle: '🤣 mabye you can make one of the jokes like i did JK hahahahahah! 😆',
-            description: '😭 me my mom telled me that there was no more Ice Cream.🍨 the next day i saw all the kids eating Ice Cream without me!🍦 ME:',
-            imageUrl: 'https://imgs.search.brave.com/Lpcf4XCsGQuJsqffHbMzZwL3_ovK54LRuXAVvNbGD_0/rs:fit:1200:1080:1/g:ce/aHR0cDovL2dldHdh/bGxwYXBlcnMuY29t/L3dhbGxwYXBlci9m/dWxsL2IvMS83Lzk2/MDcyMy13aWRlc2Ny/ZWVuLXRoZS1hbWF6/aW5nLXdvcmxkLW9m/LWd1bWJhbGwtd2Fs/bHBhcGVycy0xOTIw/eDEwODAtZm9yLWRl/c2t0b3AuanBn',
-            address: '10th joke road 778901 New York',
-            contactEmail: 'joke@test.com',
+            title: 'Fire 🤤',
+            subtitle: 'dO yOu LiKe FiRe 🤤',
+            description: 'hmm.. oh did not see you there enjoy this fire with me please 🥱',
+            imageUrl: 'https://imgs.search.brave.com/GHEBXPKUZK-9J0AuVimuEpOF9J1HD7w7l7qw1sl9wck/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/ZW1lcmdlbmN5LnBp/dHQuZWR1L3NpdGVz/L2RlZmF1bHQvZmls/ZXMvYmxvY2staW1n/L2ZpcmUuanBn',
+            address: 'FireRoad 32523 New York',
+            contactEmail: 'FireTime@test.com',
         },
     ];
         
             
            
             
-    
+    function addMeetup() {
+        const newMeetup = {
+            id: Math.random().toString(),
+            title: title,
+            subtitle: subtitle,
+            description: description,
+            imgUrl: imgUrl,
+            contactEmail: email,
+            address: address
+        };
+
+        // meetups.push(newMeetup); // DOSE NOT WORK!
+        meetups = [newMeetup, ...meetups];
+    }
 </script>
+
+<style>
+    main {
+        margin-top: 5rem;
+    }
+
+    form {
+        width: 30rem;
+        max-width: 90%;
+        margin: auto;
+    }
+</style>
 
 <Header />
 
-{#each meetups as meetup}
-    <MeetupItem />
-{/each}
+<main>
+    <form on:submit|preventDefault="{addMeetup}">
+       <TextInput 
+       id="title" 
+       label="Title" 
+       type="text"
+       value={title}
+       on:input="{event => (title = event.target.value)}" />
+       <TextInput 
+       id="subtitle" 
+       label="Subtitle" 
+       type="text"
+       value={subtitle}
+       on:input="{event => (subtitle = event.target.value)}" />
+       <TextInput 
+       id="address" 
+       label="Address" 
+       type="text"
+       value={address}
+       on:input="{event => (address = event.target.value)}" />
+       <TextInput 
+       id="imgUrl" 
+       label="ImgUrl" 
+       type="text"
+       value={imgUrl}
+       on:input="{event => (imgUrl = event.target.value)}" />
+       <TextInput 
+       id="email" 
+       label="E-mail" 
+       type="email"
+       value={email}
+       on:input="{event => (email = event.target.value)}" />
+       <TextInput 
+       id="description" 
+       label="Description" 
+       controlType="textarea"
+       value={description}
+       on:input="{event => (description = event.target.value)}" />
+           <div class="form-control">
+            <label for="title">Title</label>
+            <input type="text" id="title" bind:value={title} />
+        </div>
+        <div class="form-control">
+            <label for="subtitle">Subtitle</label>
+            <input type="text" id="subtitle" bind:value={subtitle} />
+        </div>
+        <div class="form-control">
+            <label for="address">Address</label>
+            <input type="text" id="address" bind:value={address} />
+        </div>
+        <div class="form-control">
+            <label for="imgUrl">Image Url</label>
+            <input type="text" id="imgUrl" bind:value={imgUrl} />
+        </div>
+        <div class="form-control">
+            <label for="email">E-Mail</label>
+            <input type="email" id="email" bind:value={email} />
+        </div>
+        <div class="form-control">
+            <label for="description">Description</label>
+            <textarea rows="3" id="description" bind:value={description} />
+        </div>
+        <button type="submit" Save></button>
+    </form>
+    <MeetupGrid {meetups} />
+</main>
