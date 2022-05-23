fa54b38af108e9df2361d9f9ec15785c558d2678
nena streeter <nenastreeter@gmail.com>
2022-05-23 14:54:23 -0500

    ANIMATION STUFF PRACTICE!

--- src/routes/nenasudemy.svelte
+++ src/routes/nenasudemy.svelte
@@ -1,63 +1,80 @@
 <!-- ACTUALLY FOLLOWING UDEMY STUFF -->
 
-
 <script>
-  import meatups from "$lib/nenasudemy/meatups-store.js";
-	import Hederr from "$lib/nenasudemy/Hederr.svelte";
-	import MeatUpGrid from "$lib/nenasudemy/MeatUpGrid.svelte";
-	// import TextInput from "$lib/nenasudemy/TextInput.svelte";
-	import Button from "$lib/nenasudemy/Button.svelte";
-	import EditNewMeatup from "$lib/nenasudemy/EditNewMeatup.svelte";
-  import MeatUpDetails from "$lib/nenasudemy/MeatUpDetails.svelte";
-		
-	// let meatups = ;
+  import { writable } from "svelte/store";
+  import { tweened } from 'svelte/motion';
+  import { cubicIn } from 'svelte/easing';
+  import { fade, fly, slide, scale } from 'svelte/transition';
+  import { flip } from 'svelte/animate';
 
-  let editMode;
-  let editedId;
-  let page = "overview";
-  let pageData = {};
+  import Spring from "$lib/nenasudemy/Spring.svelte";
 
-  function savedMeatup(event) {
-    editMode = null;
-    editedId = null;
+  let boxInput;
+  let showParagraph = false;
+
+  const progress = tweened(0, {
+    delay: 0,
+    duration: 700,
+    easing: cubicIn,
+  });
+
+  setTimeout(() => {
+    progress.set(0.5);
+  }, 1500);
+
+  let boxes = [];
+
+  function addBox() {
+    boxes = [boxInput.value, ...boxes];
   }
 
-  function cancelEdit() {
-    editMode = null;
-    editedId = null;
+  function discard(value) {
+    boxes= boxes.filter(el => el !== value);
   }
 
-  function showDetails(event) {
-    page = "details";
-    pageData.id = event.detail;
-  }
-
-  function closeDetails() {
-    page = 'overview';
-    pageData = {};
-  }
-
-  function startEdit(event) {
-    editMode = 'edit';
-    editedId = event.detail;
-  }
 </script>
 
 <style>
-  main {
-    margin-top: 5rem;
+  div {
+    width: 10rem;
+    height: 10rem;
+    background: rgb(187, 187, 207);
+    margin: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.40);
+    border-radius: 10px;
+    padding: 1rem;
+    text-align: center;
   }
 </style>
 
-<Hederr />
+<!-- <progress value={$progress}></progress> -->
+<!-- <Spring/> -->
 
-<main>
-  {#if page === 'overview'}
-    {#if editMode === 'edit'}
-      <EditNewMeatup id={editedId} on:save={savedMeatup} on:cancel={cancelEdit} />
-    {/if}
-    <MeatUpGrid meatups={$meatups} on:showDetails={showDetails} on:edit={startEdit} on:add={() => {editMode = 'edit'}} />
-  {:else}
-    <MeatUpDetails id={pageData.id} on:close={closeDetails} />
-  {/if}
-</main>
+<button 
+  on:click={() => {
+    showParagraph =!showParagraph}}>
+  Toggle
+</button>
+
+{#if showParagraph}
+  <p in:fade out:fly={{x:300}}>Can you see me?</p>
+{/if}
+
+<hr >
+
+<input type="text" bind:this={boxInput}>
+<button on:click={addBox}>Add</button>
+
+{#if showParagraph}
+  {#each boxes as box (box)}
+    <div 
+      transition:fly|local={{x: -200, y:0}} 
+      on:click={discard.bind(this, box)}
+      on:introstart={() => console.log('Adding Element Starts')}
+      on:introend={() => console.log('Adding Element Ends')}
+      on:outrostart={() => console.log('Removing Element Starts')}
+      on:outroend={() => console.log('Removing Element Ends')}
+      animate:flip={{duration: 400}}>
+    {box}</div>
+  {/each}
+{/if}
