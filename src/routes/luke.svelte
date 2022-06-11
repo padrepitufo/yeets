10c578083edacbd5c74d20e78d7c5e4c8b255058
Luke Streeter <lukecharlesstreeter@gmail.com>
2022-06-10 20:15:30 -0500

    Posted something

--- src/routes/luke.svelte
+++ src/routes/luke.svelte
@@ -5,84 +5,14 @@
     import TextInput from "$lib/LukesMeetups/TextInput.svelte";
     import Button from "$lib/LukesMeetups/Button.svelte";
     import EditMeetup from "$lib/LukesMeetups/EditMeetup.svelte";
+    import Cart from "$lib/LukesMeetups/Cart.svelte";
+    import Products from "$lib/LukesMeetups/Products.svelte";
 
-    let meetups = [
-    {
-      id: "m1",
-      title: "Coding Bootcamp",
-      subtitle: "Learn to code in 2 hours",
-      description:
-        "In this meetup, we will have some experts that teach you how to code!",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG/800px-Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG",
-      address: "27th Nerd Road, 32523 New York",
-      contactEmail: "code@test.com",
-      isFavorite: false
-    },
-    {
-      id: "m2",
-      title: "Swim Together",
-      subtitle: "Let's go for some swimming",
-      description: "We will simply swim some rounds!",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg",
-      address: "27th Nerd Road, 32523 New York",
-      contactEmail: "swim@test.com",
-      isFavorite: false
-    }
-  ];
-
-  let editMode;
-
-  function addMeetup(event) {
-    const newMeetup = {
-      id: Math.random().toString(),
-      title: event.detail.title,
-      subtitle: event.detail.subtitle,
-      description: event.detail.description,
-      imageUrl: event.detail.imageUrl,
-      contactEmail: event.detail.email,
-      address: event.detail.address
-    };
-
-    // meetups.push(newMeetup); // DOES NOT WORK!
-    meetups = [newMeetup, ...meetups];
-    editMode = null;
-  }
-
-  function cancelEdit() {
-    editMode = null;
-  }
-
-  function toggleFavorite(event) {
-    const id = event.detail;
-    const updatedMeetup = { ...meetups.find(m => m.id === id) };
-    updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
-    const meetupIndex = meetups.findIndex(m => m.id === id);
-    const updatedMeetups = [...meetups];
-    updatedMeetups[meetupIndex] = updatedMeetup;
-    meetups = updatedMeetups;
-  }
+let showCart = true;
 </script>
 
-<style>
-  main {
-    margin-top: 5rem;
-  }
-
-  .meetup-controls {
-    margin: 1rem;
-  }
-</style>
-
-<Hedar />
-
-<main>
-  <div class="meetup-controls">
-    <Button on:click={() => (editMode = 'add')}>New Meetup</Button>
-  </div>
-  {#if editMode === 'add'}
-    <EditMeetup on:save={addMeetup} on:cancel={cancelEdit} />
-  {/if}
-  <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
-</main>
+<Button on:click={() => {showCart = !showCart}}>Togglec Cart</Button>
+{#if showCart}
+<Cart />
+{/if}
+<Products /> 
